4df45a6aa3e2485a8df8b2a73e9ada3429cfedef
Stephen M <stephen.m@optisolbusiness.com>
2020-03-03 17:58:28 +0530

    elastic search with docker inital commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    collection do
+      get :search
+    end
+  end
+end
